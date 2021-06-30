404cffd3e20ae5b31b4f529f2591ee1d5633d7a3
karenw195-square <64095703+karenw195-square@users.noreply.github.com>
2021-06-30 16:52:24 -0700

    Update and rename current_terminal_seller_p_and_l to current_terminal_seller_p_and_l.sql

--- current_terminal_seller_p_and_l.sql
+++ current_terminal_seller_p_and_l.sql
@@ -1,3 +1,4 @@
+/*payment attribution flow chart: https://docs.google.com/drawings/d/1j0z-W6zSG73oPrm41F-0djFGvmhhp0QUp99r_49wPk8/edit*/
 select distinct merchant_token
 from app_bi.app_bi_dw.vfact_merchant_revenue_summary
 where product_name = 'Register Terminal'
