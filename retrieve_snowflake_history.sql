8882bb66749d06929e89f0b8a0e30c8da59295ca
karenw195-square <64095703+karenw195-square@users.noreply.github.com>
2022-04-25 11:27:56 -0700

    Update retrieve_snowflake_history.sql

--- retrieve_snowflake_history.sql
+++ retrieve_snowflake_history.sql
@@ -8,3 +8,19 @@ WHERE user_name = 'KARENWANG'
   AND start_time >= '2021-04-01'
 ORDER BY start_time DESC
 ;
+
+
+select
+    coalesce(nullif(user_name,'LOOKER'),upper(query_tag)) as user_name
+    ,WAREHOUSE_NAME
+    ,count(1) as frequency
+    ,max(start_time) as last_used_time
+from 
+   snowflake_usage.account_usage.query_history
+where
+    lower(query_text) like '%customer_data.cdp_event_logger.merchant_complete_payment%'
+and start_time >= dateadd(day,-365,current_date)
+group by
+    1,2
+order by
+    2 desc
